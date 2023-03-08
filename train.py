414ee442b2d38cb1e5f07907da9d40b3459217d2
Tao Roa Lin <taoroalin@gmail.com>
2023-03-08 18:29:04 +0000

    some more whistles

--- train.py
+++ train.py
@@ -70,7 +70,7 @@ def train(
     ckpt_dir: str,
     tokenizer_path: str,
     train_json_file: str,
-    lr: float = 1e-5,
+    lr: float = 3e-5,
     max_seq_len: int = 2048,
     max_batch_size: int = 32,
     truncate_seq_len:bool=False,
@@ -83,7 +83,12 @@ def train(
         sys.stdout = open(os.devnull, "w")
     train_json = json.load(open(train_json_file))
     
-    # random.shuffle(train_json)
+    same_perm_start_len = 50
+    perm = torch.randperm(len(train_json)-same_perm_start_len)
+    new_train_json = train_json[:same_perm_start_len]
+    for i,p in enumerate(perm):
+        new_train_json.append(train_json[p+same_perm_start_len])
+    train_json=new_train_json
     
     if truncate_seq_len:
         for i,x in enumerate(train_json):
@@ -107,12 +112,9 @@ def train(
         data_and_mask = [(x[0][:,:max_seq_len],x[1]) for x in data_and_mask]
     data_and_mask_filtered = [x for x in data_and_mask if x[0].shape[1]<=max_seq_len]
     
-    # wandb init
     if local_rank==0:
         wandb.init(
-            # set the wandb project where this run will be logged
             project="llama-7b-finetune",
-            # track hyperparameters and run metadata
             config={
                 "learning_rate": lr,
                 "architecture": ckpt_dir,
@@ -123,6 +125,7 @@ def train(
         )
     try:
         for i,(dp, pre) in enumerate(data_and_mask_filtered):
+            stime=time.time()
             ln = dp.shape[1]
             optimizer.zero_grad()
             out = model.forward_train_mode(dp, 0)
@@ -131,10 +134,10 @@ def train(
             if warmup_steps is not None and i<warmup_steps:
                 optimizer.lr=lr*(i+1)/(warmup_steps+1)
             else:
-                optimizer.lr=lr*math.cos(i/len(data_and_mask_filtered)*math.pi/2)
+                optimizer.lr=lr*math.cos(i/(len(data_and_mask_filtered)*1.2)*math.pi/2)
             optimizer.step()
             if local_rank==0:
-                wandb.log({"loss": loss.detach().cpu().item(),"len":ln,"pre":pre,"lr":optimizer.lr,**{f"loss_pos{i}":x for i,x in enumerate(loss_pos)}})
+                wandb.log({"loss": loss.detach().cpu().item(),"len":ln,"pre":pre,"lr":optimizer.lr,"interval":time.time()-stime, **{f"loss_pos{i}":x for i,x in enumerate(loss_pos) if (i-1)%800==0}})
             if i!=0 and i%500==0:
                 save_model(model,save_dir+f"/steps{i}",local_rank,params)
     except Exception as e:
@@ -147,8 +150,7 @@ def compute_loss(out, tokens, mask_pre):
     out = out[:, mask_pre - 1 : -1]
     logprobs = torch.log_softmax(out, dim=-1)
     on_correct = torch.gather(logprobs, 2, tokens.unsqueeze(-1))
-    pos_section_len = 400
-    on_correct_by_pos = on_correct[0,:pos_section_len*int(on_correct.shape[1]/pos_section_len),0].reshape(-1,pos_section_len).detach().cpu().mean(dim=1)
+    on_correct_by_pos = on_correct[0,:,0].detach().cpu()
     loss = on_correct.mean()
     return -loss, -on_correct_by_pos
 
